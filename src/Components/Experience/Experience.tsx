9e62dc3bcbf815a913546638efe0fe477c6aee8c
joe.kim@vidoori.com <joeekimm@gmail.com>
2023-08-02 18:09:17 -0700

    added experimental carousel style for experience and projects section

--- src/Components/Experience/Experience.tsx
+++ src/Components/Experience/Experience.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import './Experience.scss';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const Experience = () => {
   return (
-    <div>Hi, this is the experience page</div>
+    <div>
+      <h1>Experience</h1>
+      {/* <div className='experience-container'> */}
+      <Carousel>
+        <div className='xp1'>
+          Vidoori
+          <span>
+
+          </span>
+        </div>
+        <div className='xp2'>
+          Hack Reactor
+        </div>
+      </Carousel>
+      {/* </div> */}
+      
+      <h1>Projects</h1>
+      {/* <div className='projects-container'> */}
+      <Carousel>
+        <div className='proj1'>
+          Project 1
+        </div>
+        <div className='proj2'>
+          Project 2
+        </div>
+      </Carousel>
+      {/* </div> */}
+    </div>
   )
 }
 
